31f64324c0c6ad63a1dd965175c8f73f0b19dfa0
Michael Harris <mhguitar1997@gmail.com>
2020-03-11 13:04:47 -0700

    Scaffold room and player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player():
+    def __init__(self, name, currentRoom = None):
+        self.name = name
+        self.currentRoom = currentRoom
+
+    
+    def move(self, direction):
+        if direction == "n":
+            self.currentRoom = self.currentRoom.n_to
+        elif direction == "s":
+            self.currentRoom = self.currentRoom.s_to 
+        elif direction == "e":
+            self.currentRoom = self.currentRoom.e_to 
+        elif direction == "w":
+            self.currentRoom = self.currentRoom.w_to  
